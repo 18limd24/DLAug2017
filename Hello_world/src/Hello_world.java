9793c7f2e443a878a4b889f41238cee17ae0947b
18limd24 <18limd24@millshighsch.org>
2017-08-22 09:28:11 -0700

    Second project using Git from Eclipse

---
+++ Hello_world/src/Hello_world.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ * @author Darren Lim
+ * @version 8/21/17
+ */
+public class Hello_world {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World! From Darren");
+		//Display the setting
+	}
+}
