c719723e186a5dc5bc704ed5bf6c772984079fff
18limd24 <18limd24@millshighsch.org>
2017-08-29 08:13:01 -0700

    commit 8/29

---
+++ Pikachu/src/Pikachu.java
@@ -0,0 +1,13 @@
+
+/*Darren Lim
+August 28, 2017
+This is the Pikachu warm-up */
+
+public class Pikachu {
+	public static void main(String[] args) {
+		System.out.println("\tPikachu welcomes you to the world of Pokemon!");
+		System.out.println("\t\t\t  (\\__/)");
+		System.out.println("\t\t\t  (o^.^)");
+		System.out.println("\t\t\tz(_(\")(\")");
+	}
+}
