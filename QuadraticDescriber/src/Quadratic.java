fe808fd819b350878ce4fe49bae493e19b0677cc
18limd24 <18limd24@millshighsch.org>
2017-10-10 09:07:40 -0700

    started part 5

---
+++ QuadraticDescriber/src/Quadratic.java
@@ -0,0 +1,11 @@
+//Darren Lim
+//10/10/17
+//Part 5 of calculate
+//Quadratic will be responsible for all calculations and must contain quadrDescriber
+public class Quadratic {
+	public static String quadrDescriber(double a, double b, double c) {
+		System.out.println(a + "x^2 + " + b + "x + " + c);
+		
+	}
+
+}
---
+++ QuadraticDescriber/src/QuadraticClient.java
@@ -0,0 +1,20 @@
+/*Darren Lim
+ * 10/10/17
+ * This is part 5 of our Calculate project.
+ * QuadraticClient will contain a main method and handle interactions with the user
+ */
+import java.util.*;//for Scanner
+public class QuadraticClient {
+	public static void main(String[] args) {
+		Scanner console = new Scanner(System.in);
+		System.out.println("Welcome to the Quadratic Describer");
+		System.out.println("Provide values for coefficients a, b, and c");
+		double a = console.nextDouble();
+		double b = console.nextDouble();
+		double c = console.nextDouble();
+		String description = quadrDescriber(a,b,c);
+		System.out.print(description);
+		//ask if they want to go again
+	}
+
+}
