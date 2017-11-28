299ce42aec261887630f58676ffb576b05e43095
18limd24 <18limd24@millshighsch.org>
2017-11-28 09:29:30 -0800

    in class exercise

---
+++ Code Magnets/src/DrumKit.java
@@ -0,0 +1,14 @@
+//Darren Lim
+//November 28, 2017
+//Code Magnets in class exercise
+public class DrumKit {
+	boolean topHat = true;
+	boolean snare = true;
+	void playSnare(){
+		System.out.println("bang bang ba-bang");
+	}
+	void playTopHat() {
+		System.out.println("ding ding da-ding");
+	}
+
+}
---
+++ Code Magnets/src/DrumKitTestDrive.java
@@ -0,0 +1,15 @@
+//Darren Lim
+//November 28,2017
+//Code Magnet project
+public class DrumKitTestDrive {
+	public static void main(String[] args) {
+		DrumKit d = new DrumKit();
+		d.playSnare();
+		d.playTopHat();
+		d.snare = false;
+		if(d.snare == true) {
+			d.playSnare();
+		}
+	}
+
+}
