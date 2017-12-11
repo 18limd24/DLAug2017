f4016b9bfdfe01321a2468b4445f3399059058f7
Darren <Darren@LimFamily-HP.attlocal.net>
2017-12-10 21:42:22 -0800

    FINISHED but didn't do extra credit

--- FracCalc/src/fracCalc/FracCalc.java
+++ FracCalc/src/fracCalc/FracCalc.java
@@ -9,13 +9,13 @@ public class FracCalc {
 
     public static void main(String[] args) {
         // TODO: Read the input from the user and call produceAnswer with an equation
-    	System.out.println("Enter calculations");
+    	System.out.println("Enter calculations"); //Asks user for calculations
     	Scanner console = new Scanner(System.in);
     	//String firstInput = console.nextLine();
     	//System.out.println(produceAnswer(firstInput));
 		String input = console.nextLine();
     	
-    	while(!input.equals("quit")) {
+    	while(!input.equals("quit")) {//continues to run until user enters quit
 
     		System.out.println(produceAnswer(input));
     		System.out.println("Type 'quit' to exit, otherwise, enter new calculations");
@@ -36,92 +36,66 @@ public class FracCalc {
     //      e.g. return ==> "1_1/4"
     public static String produceAnswer(String input){ 
         // TODO: Implement this function to produce the solution to the input
-        String[] threeStrings = input.split(" ");
-        String operand1 = threeStrings[0];
+        String[] threeStrings = input.split(" ");//stores the input into an array
+        String operand1 = threeStrings[0];//easier to read
         String operator = threeStrings[1];
         String operand2 = threeStrings[2];
-        //String[] mixedN1 = splitOperand(operand1);//can just simplify
-        //String[] mixedN2 = splitOperand(operand2);
+        //split the operands into whole, numerator, and denominator and then parse to integers
         int[] mixedN1 = {Integer.parseInt(splitOperand(operand1)[0]),Integer.parseInt(splitOperand(operand1)[1]), Integer.parseInt(splitOperand(operand1)[2])};
         int[] mixedN2 = {Integer.parseInt(splitOperand(operand2)[0]),Integer.parseInt(splitOperand(operand2)[1]),Integer.parseInt(splitOperand(operand2)[2])};
+        //store as improper fractions to do calculations
         int[] frac1 = toImproperFrac(mixedN1);
         int[] frac2 = toImproperFrac(mixedN2);
-        //if(frac1[1] != frac2[1]) {
+        //change fracs to common denominator for calculations
         commonDenom(frac1,frac2);
-        
-        //}
+        //calculations: one to do addition/subtraction, one for multiplication/division
         int[] answer = new int[2];
         if(operator.equals("+")) {
         	answer = addition(frac1,frac2);
-        }else if(operator.equals("-")) {
+        }else if(operator.equals("-")) {//subtraction is negative addition
         	frac2[0] *= -1;
         	answer = addition(frac1, frac2);
         }else if(operator.equals("*")) {
         	answer = multiplication(frac1,frac2);
-        }else if(operator.equals("/")) {
+        }else if(operator.equals("/")) {//division is multiplying by reciprocal
         	int temp = frac2[0];
         	frac2[0] = frac2[1];
         	frac2[1] = temp;
         	answer = multiplication(frac1,frac2);
         }
-       reduceFrac(answer);
-        /*if(answer[0] == 0) {
-        	return "0";
-        } else if(answer[1] == 1) {
-        	return "" + answer[0];
-        }else if(answer[0] > 0) {
-        	if(answer[0] < answer[1]) {
-        		return answer[0] + "/" + answer[1];
-        	} else {
-        		int[] mixedAnswer = toMixedN(answer);
-        		return mixedAnswer[0] + "_" + mixedAnswer[1] + "/" + mixedAnswer[2];
-        	}
-    	}else {
-    		if(absValue(answer[0]) < absValue(answer[1])) {
-    			return answer[0] + "/" + answer[1];
-    		}else {
-    			int[] mixedAnswer = toMixedN(answer);
-    			return mixedAnswer[0] + "_" + -mixedAnswer[1] + "/" + mixedAnswer[2];
-    		}
-    	}*/
-        return answer[0] + "/" + answer[1];
+        //reduce answer
+       int[] reducedAnswer = reduceFrac(answer);
+        
+       if(reducedAnswer[0] == 0 && reducedAnswer[1] == 0) {//if equivalent to 0
+    	   return "0";
+       }else if(reducedAnswer[0] == 0 && reducedAnswer[1] != 0) {//if whole is 0 then only fraction
+    	   if(reducedAnswer[2] < 0) {//if denominator is negative, return absValue and multiply numerator by -1
+    		   return -reducedAnswer[1] + "/" + Math.abs(reducedAnswer[2]);
+    	   }else {
+    		   return reducedAnswer[1] + "/" + reducedAnswer[2];
+    	   }
+       }else if(reducedAnswer[0] != 0 && reducedAnswer[1] == 0) {//if only whole number
+    	   return "" + reducedAnswer[0];
+       }else {//if the whole thing
+    	   return reducedAnswer[0] + "_" + Math.abs(reducedAnswer[1]) + "/" + Math.abs(reducedAnswer[2]);
+       }
     }
-	public static void reduceFrac(int[] unreducFrac) {
+	public static int[] reduceFrac(int[] unreducFrac) {
 		//first gets gcf
-		int greaterN = 0;
-		int lesserN = 0;
-		if(absValue(unreducFrac[0]) > Math.abs(unreducFrac[1])) {
-			greaterN = unreducFrac[0];
-			lesserN = unreducFrac[1];
-		}else if(absValue(unreducFrac[0]) < Math.abs(unreducFrac[1])) {
-			greaterN = unreducFrac[1];
-			lesserN = unreducFrac[0];
-		}
-		int num = 1;
-		if(unreducFrac[0] > 1) {
-			while (lesserN != 0) {
-				num = greaterN % lesserN;
-				greaterN = lesserN;
-				lesserN = num;
-			}
-		}else {
-			while(lesserN != 0) {
-				num = absValue(greaterN % lesserN);
-				greaterN = absValue(lesserN);
-				lesserN = num;
-			}
-		}
-		if(greaterN != 0) {
-			unreducFrac[0] = unreducFrac[0]/ greaterN;
-			unreducFrac[1] = unreducFrac[1]/ greaterN;
-		}
 		
+		int[] mixedFrac = toMixedN(unreducFrac);
+		int gcf = gcf(Math.abs(mixedFrac[1]), Math.abs(mixedFrac[2]));
+		mixedFrac[1] /= gcf;
+		mixedFrac[2] /= gcf;
+		return mixedFrac;
 		
 		
     // TODO: Fill in the space below with any helper methods that you think you will need	
 	}
 
 	public static String[] splitOperand(String operand) {
+		//splitOperand will take a string and use split to split into the whole, numerator, and denominator
+		//using substrings
 		String whole = "";
 		String numerator = "";
 		String denominator = "";
@@ -153,18 +127,21 @@ public class FracCalc {
 		if(mixedN[0] < 0) {
 			mixedN[1] *= -1;
 		}
+		//multiply whole by denominator and add to the numerator
+		//2 index
 		topN += mixedN[1];
 		int[] improperFrac = {topN, mixedN[2]};
 		return improperFrac;
 	}
 	public static void commonDenom(int[] frac1, int[] frac2) {
+		//multiplies fraction by others denominator
 		frac1[0] *= frac2[1];
 		int temp = frac1[1];
 		frac1[1] *= frac2[1];
 		frac2[0] *= temp;
 		frac2[1] *= temp;
 	}
-	
+	//addition and multiplication methods
 	public static int[] addition(int[] frac1,int[] frac2) {
 		int[] answer = {frac1[0] + frac2[0], frac1[1]};
 		return answer;
@@ -174,7 +151,7 @@ public class FracCalc {
 		return answer;
 	}
 	public static int gcf(int greaterN, int lesserN) {
-		//if(absValue(greaterN) 
+		//from calculate library
 		int a = greaterN;
 		int b = lesserN;
 		int result;
@@ -183,7 +160,7 @@ public class FracCalc {
 			a = b;
 			b = result;
 		}
-		return a;
+		return a;//a will be the gcf
 	}
 
 	public static int[] toMixedN(int[] improperAnswer) {
@@ -194,15 +171,7 @@ public class FracCalc {
 		mixedAnswer[2] = improperAnswer[1];
 		return mixedAnswer;
 	}
-	public static int absValue(int a) {
-		if (a > 0) {
-			return a;
-		} else if (a == 0) {
-			return 0;
-		} else {
-			return -a;
-		}
-	}
+	
 
         
 
