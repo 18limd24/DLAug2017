3ee1fdd58effbadaa42b679cb7a6458a0f309800
18limd24 <18limd24@millshighsch.org>
2018-03-12 08:54:02 -0700

    starting textexcel

---
+++ TextExcel/src/textExcel/Spreadsheet.java
@@ -0,0 +1,27 @@
+package textExcel;
+
+// Update this file with your own code.
+
+public class Spreadsheet implements Grid{
+
+	public String processCommand(String command){
+		return null;
+	}
+
+	public int getRows(){
+		return 0;
+	}
+
+	public int getCols(){
+		return 0;
+	}
+
+	public Cell getCell(Location loc){
+		return null;
+	}
+
+	public String getGridText(){
+		return null;
+	}
+
+}
---
+++ TextExcel/src/textExcel/SpreadsheetLocation.java
@@ -0,0 +1,18 @@
+package textExcel;
+
+//Update this file with your own code.
+
+public class SpreadsheetLocation implements Location{
+    public int getRow(){
+        return 0;
+    }
+
+    public int getCol(){
+        return 0;
+    }
+    
+    public SpreadsheetLocation(String cellName){
+        
+    }
+
+}
